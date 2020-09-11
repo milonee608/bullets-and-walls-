223b334ad5166b7f76851aa6957a7b919254db7d
milonee608 <70271042+milonee608@users.noreply.github.com>
2020-09-11 20:19:27 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,43 @@
+var bullet,wall;
+var thickness,speed,weight;
+function setup() {
+  createCanvas(1600,400);
+  //createSprite(400, 200, 50, 50);
+
+  speed=random(223,321);
+  weight=random(30,52);
+  thickness=random(22,83);
+
+  bullet=createSprite(50,200,50,50);
+  bullet.velocityX=speed;
+
+  wall=createSprite(1500,200,thickness,height/2);
+  wall.shapeColor=color(80,80,80);
+  
+}
+function hasCollided(ibullet,iwall){
+  bulletRightEdge=ibullet.x-ibullet.width;
+  wallLeftEdge=iwall.x;
+  if(bulletRightEdge>=wallLeftEdge){
+    return true;  
+  }
+  return false;
+}
+function draw() {
+  background(0); 
+  
+if(hasCollided(bullet,wall))
+{
+  bullet.velocityX=0;
+  var damage=0.5*weight*speed*speed/(thickness*thickness*thickness);
+  if(damage>10)
+  {
+    wall.shapeColor=color(255,0,0);
+  }
+  if(damage<10)
+  {
+    wall.shapeColor=color(0,255,0);
+  }
+}
+  drawSprites();
+}
